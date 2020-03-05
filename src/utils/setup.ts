0fe305ad9b642082692b7f295dac78d1c6dc6e00
petersowa <petersowa@users.noreply.github.com>
2020-03-04 21:03:34 -0600

    add env check to setup

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
-const result = dotenv.config();
-if (result.error) {
-	throw result.error;
+if (process.env.NODE_ENV !== 'production') {
+	const result = dotenv.config();
+	if (result.error) {
+		throw result.error;
+	}
 }
