13717e219b58c6afb3976faf6a656fe09f2bbacb
petersowa <petersowa@users.noreply.github.com>
2020-03-09 20:12:55 -0500

    setup typescript auth test

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,14 @@
 import * as fastify from 'fastify';
+
 import { Server, IncomingMessage, ServerResponse } from 'http';
 
 async function routes(fastify: fastify.FastifyInstance, options: {}) {
-	fastify.get('/test', async (request, reply) => {
-		return { data: 'test' };
-	});
+	fastify.get<Server, IncomingMessage, ServerResponse>(
+		'/test',
+		async (request, reply) => {
+			return { data: 'test' };
+		}
+	);
 }
 
 export default routes;
