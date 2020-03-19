4a1eedf5b33e1f1a99170f9626a35b8c0888fa9c
petersowa <petersowa@users.noreply.github.com>
2020-03-18 21:37:58 -0500

    add client testing for simple socket-io

--- public/js/test.js
+++ public/js/test.js
@@ -1,1 +1,14 @@
-alert('test');
+var socket = io();
+
+socket.on('connect', () => {
+	console.log('connected');
+	socket.emit('query', 'query data');
+});
+
+socket.on('update', msg => {
+	console.log(msg);
+});
+
+document.getElementById('query-button').addEventListener('click', () => {
+	socket.emit('query', 'button clicked');
+});
