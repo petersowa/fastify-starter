5741519f3787e6abd83eec3f9d3e8c04530194bd
petersowa <petersowa@users.noreply.github.com>
2020-03-30 22:24:54 -0500

    add dynamic data to template

--- src/routes/template.ts
+++ src/routes/template.ts
@@ -1,10 +1,17 @@
 import fastify from 'fastify';
 import pov from 'point-of-view'; // import for type support
 
+const article = `
+An important change is that the Tab key is no longer the default way to expand Emmet abbreviations. Instead, Emmet abbreviations will now appear in the suggestion list. They can be selected like any other smart completion and on selection, the abbreviation will be expanded.
+
+We can quickly view the Emmet abbreviation by clicking the info icon next to list shown. By typing text next to hash (#) will be taken as id and text next to period(.) will be considered as class name. Emmet basically works related to CSS selectors.	
+`;
+
 async function routes(fastify: fastify.FastifyInstance, options: {}) {
 	fastify.get('/', (request, reply) => {
 		reply.view('./views/layout', {
 			name: 'home page',
+			article,
 		});
 	});
 }
