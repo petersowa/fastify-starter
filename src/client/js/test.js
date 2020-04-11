b341c8f262ff5662b46c4ebc980a98dc71a89f5c
petersowa <petersowa@users.noreply.github.com>
2020-04-10 22:47:23 -0500

    remove dynamic login button

--- src/client/js/test.js
+++ src/client/js/test.js
@@ -6,21 +6,6 @@ const formSubmit = document.getElementById('form-submit');
 const loginButton = document.getElementById('login-button');
 const modal = document.getElementById('modal');
 
-const loginForm = `
-<form id="login-form" class="login-form" action='/auth/login' method="post">
-	Login Form 2
-	<input type="text" placeholder="email" name="username" />
-	<input type="password" placeholder="password" name="password" />
-	<div class="h-controls">
-		<button type="submit">Submit</button>
-		<button id="login-cancel-btn" type="button">
-			Cancel
-		</button>
-	</div>
-	<input type="hidden" value="" name="_csrf" />
-</form>
-`;
-
 socket.on('connect', () => {
 	console.log('connected');
 	socket.emit('query', 'query data');
@@ -35,9 +20,9 @@ document.getElementById('query-button').addEventListener('click', () => {
 });
 
 formCancelBtn.addEventListener('click', () => {
-	modal.classList.remove('modal--show');
-	setTimeout(() => modal.classList.remove('modal--hide'), 300);
-	// window.history.back();
+	// modal.classList.remove('modal--show');
+	// setTimeout(() => modal.classList.remove('modal--hide'), 300);
+	window.history.back();
 });
 
 formSubmit.addEventListener('submit', (event) => {
@@ -50,8 +35,8 @@ formSubmit.addEventListener('submit', (event) => {
 	console.log('submit');
 });
 
-loginButton.addEventListener('click', () => {
-	// modal.innerHTML = loginForm;
-	modal.classList.add('modal--hide');
-	setTimeout(() => modal.classList.add('modal--show'), 10);
-});
+// loginButton.addEventListener('click', () => {
+// 	// modal.innerHTML = loginForm;
+// 	modal.classList.add('modal--hide');
+// 	setTimeout(() => modal.classList.add('modal--show'), 10);
+// });
