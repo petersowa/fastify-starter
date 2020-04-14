25298d3c2df91705b9e93ee855ebb7fa60a53cd3
petersowa <petersowa@users.noreply.github.com>
2020-04-13 23:11:33 -0500

    config for svelte and css

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,11 +1,91 @@
+// base on https://github.com/sveltejs/template-webpack/blob/master/webpack.config.js
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
 module.exports = {
-	mode: 'development',
-	entry: './js/test.js',
-	output: {
-		filename: 'test.js',
-		path: path.resolve(__dirname, '..', '..', 'public', 'js'),
+	mode,
+	entry: { main: './js/test.js' },
+	resolve: {
+		alias: {
+			svelte: path.resolve('node_modules', 'svelte'),
+		},
+		extensions: ['.mjs', '.js', '.svelte'],
+		mainFields: ['svelte', 'browser', 'module', 'main'],
 	},
-	devtool: 'inline-source-map',
+	output: {
+		filename: 'js/test.js',
+		path: path.resolve(__dirname, '..', '..', 'public'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						emitCss: true,
+						hotReload: true,
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65,
+							},
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.9],
+								speed: 4,
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75,
+							},
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+		}),
+		// new CleanWebpackPlugin(),
+		new CopyWebPackPlugin([{ from: './images', to: 'images' }]),
+	],
+	devtool: prod ? false : 'inline-source-map',
 };
