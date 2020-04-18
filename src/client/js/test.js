e71379ccdc8b8628d310c384ead305f20d255d45
petersowa <petersowa@users.noreply.github.com>
2020-04-18 14:55:13 -0500

    refactor elements/events into object list

--- src/client/js/test.js
+++ src/client/js/test.js
@@ -1,17 +1,58 @@
 import io from 'socket.io-client';
-
-const socket = io();
-const formCancelBtn = document.getElementById('form-cancel');
-const formSubmit = document.getElementById('form-submit');
-const loginButton = document.getElementById('login-button');
-const modal = document.getElementById('modal');
-
 import App from './quote.svelte';
+import '../styles/main.css';
 
 const app = new App({ target: document.getElementById('app'), data: {} });
 console.log({ app });
 
-import '../styles/main.css';
+const socket = io();
+
+const elements = {
+	'form-cancel': {
+		events: {
+			click: () => {
+				// modal.classList.remove('modal--show');
+				// setTimeout(() => modal.classList.remove('modal--hide'), 300);
+				window.history.back();
+			},
+		},
+	},
+	'form-submit': {
+		events: {
+			submit: (event) => {
+				// event.preventDefault();
+				// const csrf = /_csrf=(\S*);/.exec(document.cookie)[1];
+				const csrf = document
+					.querySelector('meta[name="csrf-token"]')
+					.getAttribute('content');
+				event.target._csrf.value = csrf;
+				console.log('submit');
+			},
+		},
+	},
+	'login-button': {},
+	modal: {},
+	'query-button': {
+		events: {
+			click: () => {
+				console.log('query button click event');
+				socket.emit('query', 'button clicked' + socket.id);
+			},
+		},
+	},
+};
+
+for (const element in elements) {
+	const elem = document.getElementById(element);
+	console.log(elem);
+	if (elem) {
+		elements[element].elem = elem;
+		for (const event in elements[element].events) {
+			console.log(event, elements[element].events[event]);
+			elem.addEventListener(event, elements[element].events[event]);
+		}
+	}
+}
 
 socket.on('connect', () => {
 	console.log('connected ');
@@ -21,30 +62,3 @@ socket.on('connect', () => {
 socket.on('update', (msg) => {
 	console.log(msg);
 });
-
-// query-button no longer on page
-document.getElementById('query-button').addEventListener('click', () => {
-	socket.emit('query', 'button clicked' + socket.id);
-});
-
-formCancelBtn.addEventListener('click', () => {
-	// modal.classList.remove('modal--show');
-	// setTimeout(() => modal.classList.remove('modal--hide'), 300);
-	window.history.back();
-});
-
-formSubmit.addEventListener('submit', (event) => {
-	// event.preventDefault();
-	// const csrf = /_csrf=(\S*);/.exec(document.cookie)[1];
-	const csrf = document
-		.querySelector('meta[name="csrf-token"]')
-		.getAttribute('content');
-	event.target._csrf.value = csrf;
-	console.log('submit');
-});
-
-// loginButton.addEventListener('click', () => {
-// 	// modal.innerHTML = loginForm;
-// 	modal.classList.add('modal--hide');
-// 	setTimeout(() => modal.classList.add('modal--show'), 10);
-// });
