1661569edb9cb7dd5dfbd7eb117b5cb36dbd369f
petersowa <petersowa@users.noreply.github.com>
2020-04-21 09:39:03 -0500

    add user agent to model

--- src/models/webStats.ts
+++ src/models/webStats.ts
@@ -4,12 +4,14 @@ import { Schema, Document } from 'mongoose';
 interface WebStatsModel extends Document {
 	ip: string;
 	header: string;
+	userAgent: string;
 	count: number;
 }
 
 const schema: Schema = new Schema({
 	ip: { type: String, required: true, unique: true },
-	header: { type: String, required: true },
+	header: { type: String },
+	userAgent: { type: String },
 	count: { type: Number, default: 0 },
 });
 
