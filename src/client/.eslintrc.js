2913b523d0abb8249c628ef61d5131479655ea2a
petersowa <petersowa@users.noreply.github.com>
2020-04-20 22:01:17 -0500

    added client eslint config

---
+++ src/client/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['eslint-plugin', 'prettier'],
+	extends: ['eslint:recommended'],
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+	},
+};
