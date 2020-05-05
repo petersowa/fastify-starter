8bacf95337e4dece0fafeff0ebf1ac6efbe23aae
petersowa <petersowa@users.noreply.github.com>
2020-05-04 20:17:15 -0500

    improve watchlist display with details

--- src/client/js/Finance/WatchList.svelte
+++ src/client/js/Finance/WatchList.svelte
@@ -9,8 +9,30 @@
 	});
 </script>
 
+<style type="scss">
+	.watchlist {
+		display: flex;
+		flex-direction: column;
+		justify-content: left;
+		margin: 0;
+		width: 100%;
+		outline: 1px solid red;
+		padding: 0;
+		&__row {
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr 1fr;
+			grid-auto-rows: 1.2rem;
+		}
+	}
+</style>
+
 <ul class="watchlist">
 	{#each watchItems as quote}
-		<li>{quote.symbol}</li>
+		<li class="watchlist__row">
+			<span>{quote.symbol}</span>
+			<span>{quote.latestPrice}</span>
+			<span>{quote.peRatio}</span>
+			<span>{new Date(quote.latestUpdate).toLocaleString()}</span>
+		</li>
 	{/each}
 </ul>
