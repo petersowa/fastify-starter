60168515ddb364368cf116dc9c75b1afc1002fae
petersowa <petersowa@users.noreply.github.com>
2020-05-04 20:16:54 -0500

    add spinner from css load.net

--- src/client/js/spinner.svelte
+++ src/client/js/spinner.svelte
@@ -50,21 +50,109 @@
 
 	.modal-svelte {
 		position: absolute;
+		display: flex;
+		justify-content: center;
+		align-content: center;
 		left: 50%;
 		top: 50%;
-		width: calc(100vw - 4em);
+		width: 5em;
+		height: 5em;
 		max-width: 32em;
 		max-height: calc(100vh - 4em);
-		overflow: auto;
+		overflow: hidden;
 		transform: translate(-50%, -50%);
-		padding: 1em;
 		border-radius: 0.2em;
-		background: white;
+	}
+
+	.cssload-container {
+		position: relative;
+	}
+
+	.cssload-whirlpool,
+	.cssload-whirlpool::before,
+	.cssload-whirlpool::after {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		border: 1px solid rgba(202, 14, 177, 0.938);
+		border-left-color: rgba(238, 240, 123, 0.89);
+		border-radius: 974px;
+		-o-border-radius: 974px;
+		-ms-border-radius: 974px;
+		-webkit-border-radius: 974px;
+		-moz-border-radius: 974px;
+	}
+
+	.cssload-whirlpool {
+		margin: -24px 0 0 -24px;
+		height: 49px;
+		width: 49px;
+		animation: cssload-rotate 1150ms linear infinite;
+		-o-animation: cssload-rotate 1150ms linear infinite;
+		-ms-animation: cssload-rotate 1150ms linear infinite;
+		-webkit-animation: cssload-rotate 1150ms linear infinite;
+		-moz-animation: cssload-rotate 1150ms linear infinite;
+	}
+
+	.cssload-whirlpool::before {
+		content: '';
+		margin: -22px 0 0 -22px;
+		height: 43px;
+		width: 43px;
+		animation: cssload-rotate 1150ms linear infinite;
+		-o-animation: cssload-rotate 1150ms linear infinite;
+		-ms-animation: cssload-rotate 1150ms linear infinite;
+		-webkit-animation: cssload-rotate 1150ms linear infinite;
+		-moz-animation: cssload-rotate 1150ms linear infinite;
+	}
+
+	.cssload-whirlpool::after {
+		content: '';
+		margin: -28px 0 0 -28px;
+		height: 55px;
+		width: 55px;
+		animation: cssload-rotate 2300ms linear infinite;
+		-o-animation: cssload-rotate 2300ms linear infinite;
+		-ms-animation: cssload-rotate 2300ms linear infinite;
+		-webkit-animation: cssload-rotate 2300ms linear infinite;
+		-moz-animation: cssload-rotate 2300ms linear infinite;
+	}
+
+	@keyframes cssload-rotate {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	@-o-keyframes cssload-rotate {
+		100% {
+			-o-transform: rotate(360deg);
+		}
+	}
+
+	@-ms-keyframes cssload-rotate {
+		100% {
+			-ms-transform: rotate(360deg);
+		}
+	}
+
+	@-webkit-keyframes cssload-rotate {
+		100% {
+			-webkit-transform: rotate(360deg);
+		}
+	}
+
+	@-moz-keyframes cssload-rotate {
+		100% {
+			-moz-transform: rotate(360deg);
+		}
 	}
 </style>
 
 <div class="modal-background" />
 
 <div class="modal-svelte" role="dialog" aria-modal="true" bind:this={modal}>
-	<slot />
+	<div class="cssload-container">
+		<div class="cssload-whirlpool" />
+	</div>
 </div>
