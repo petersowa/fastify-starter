7d13789b6ed5b8f0a16285ab813fff4183a1c3fe
petersowa <petersowa@users.noreply.github.com>
2020-05-20 19:40:30 -0500

    refactor auth options for routes

--- src/routes/stocks.ts
+++ src/routes/stocks.ts
@@ -3,33 +3,15 @@ import { checkSessionAuth } from '../controllers/protected';
 import { updateWatchlist, getWatchlist } from '../controllers/watchlist';
 import { getQuote } from '../controllers/quote';
 
+const options = { preHandler: checkSessionAuth };
+
 async function routes(
 	fastify: fastify.FastifyInstance,
 	_options: {}
 ): Promise<void> {
-	fastify.get(
-		'/quote/:symbol',
-		{
-			preHandler: checkSessionAuth,
-		},
-		getQuote
-	);
-
-	fastify.get(
-		'/watchlist',
-		{
-			preHandler: checkSessionAuth,
-		},
-		getWatchlist
-	);
-
-	fastify.post(
-		'/watchlist',
-		{
-			preHandler: checkSessionAuth,
-		},
-		updateWatchlist
-	);
+	fastify.get('/quote/:symbol', options, getQuote);
+	fastify.get('/watchlist', options, getWatchlist);
+	fastify.post('/watchlist', options, updateWatchlist);
 }
 
 export default routes;
