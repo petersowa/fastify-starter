fab2e14790a6ecc4a91a934743053a253dd69173
petersowa <petersowa@users.noreply.github.com>
2020-05-27 21:58:10 -0500

    code cleanup

--- src/client/js/spinner.svelte
+++ src/client/js/spinner.svelte
@@ -1,41 +1,5 @@
 <script>
-	import { createEventDispatcher, onDestroy } from 'svelte';
-
-	const dispatch = createEventDispatcher();
-	const close = () => dispatch('close');
-
 	let modal;
-
-	const handle_keydown = e => {
-		if (e.key === 'Escape') {
-			close();
-			return;
-		}
-
-		if (e.key === 'Tab') {
-			// trap focus
-			const nodes = modal.querySelectorAll('*');
-			const tabbable = Array.from(nodes).filter(n => n.tabIndex >= 0);
-
-			let index = tabbable.indexOf(document.activeElement);
-			if (index === -1 && e.shiftKey) index = 0;
-
-			index += tabbable.length + (e.shiftKey ? -1 : 1);
-			index %= tabbable.length;
-
-			tabbable[index].focus();
-			e.preventDefault();
-		}
-	};
-
-	const previously_focused =
-		typeof document !== 'undefined' && document.activeElement;
-
-	if (previously_focused) {
-		onDestroy(() => {
-			previously_focused.focus();
-		});
-	}
 </script>
 
 <style>
@@ -49,7 +13,7 @@
 	}
 
 	.modal-svelte {
-		position: absolute;
+		position: fixed;
 		display: flex;
 		justify-content: center;
 		align-content: center;
