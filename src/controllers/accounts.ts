e5a1957d7abe1ec0455d97bbfac2fa589fe9f676
petersowa <petersowa@users.noreply.github.com>
2020-06-17 22:24:51 -0500

    add logic to update account holding db

--- src/controllers/accounts.ts
+++ src/controllers/accounts.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { ServerResponse } from 'http';
-import Account, { AccountInterface } from '../models/accountModel';
+import AccountModel, { AccountInterface } from '../models/accountModel';
+import HoldingsModel, { HoldingsInterface } from '../models/holdingsModel';
 
 async function getAccounts(
 	request: FastifyRequest,
@@ -19,12 +20,43 @@ async function updateAccount(
 	reply: FastifyReply<{}>
 ): Promise<{}> {
 	const { account, position } = request.body;
+	let userAccount = await AccountModel.findOne({
+		user: request.session.userId,
+	});
+	if (userAccount) {
+		await userAccount.populate('holdings');
+		console.log({ found: userAccount });
+	} else {
+		const newHoldings = new HoldingsModel({
+			holdings: [
+				{
+					date: new Date(),
+					type: 'stock',
+					quantity: 12,
+					cost: 9.99,
+					fees: 9.99,
+				},
+			],
+		});
+		userAccount = new AccountModel({
+			user: request.session.userId,
+			holdings: newHoldings.id,
+		});
+		try {
+			const doc = await userAccount.save();
+			const holdingDoc = await newHoldings.save();
+			console.log('created account', doc, holdingDoc);
+		} catch (err) {
+			console.log({ createNewAccountError: err });
+		}
+		console.log({ found: 'no user account' });
+	}
 	if (!account) {
 		reply.code(400);
 		return { status: 'no data' };
 	}
 	console.log({ account, position });
-	return {};
+	return { position };
 }
 
 export { updateAccount, getAccounts };
