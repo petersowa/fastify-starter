b4bd376057fbfd27c0bbf8f3cc199b68c839e730
petersowa <petersowa@users.noreply.github.com>
2020-06-26 17:10:56 -0500

    code refactor

--- src/client/js/Finance/handle-ajax.js
+++ src/client/js/Finance/handle-ajax.js
@@ -1,22 +1,34 @@
+const WATCHLIST_ROUTE = '/stocks/watchlist';
+const ACCOUNT_ROUTE = '/stocks/account';
+const QUOTE_ROUTE = '/stocks/quote';
+const POST = 'POST';
+const PATCH = 'PATCH';
+const DELETE = 'DELETE';
+const GET = 'GET';
+
 function getTokenCSRF() {
 	return document
 		.querySelector('meta[name="csrf-token"]')
 		.getAttribute('content');
 }
 
+function secureFetch(route, method, body) {
+	return fetch(route, {
+		method,
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+			'csrf-token': getTokenCSRF(),
+		},
+		body: JSON.stringify(body),
+	});
+}
+
 async function postWatchlist(list) {
 	console.log('POSTWATCHLIST');
 	try {
-		const response = await fetch(`/stocks/watchlist`, {
-			method: 'POST',
-			credentials: 'same-origin',
-			headers: {
-				'Content-Type': 'application/json',
-				'csrf-token': getTokenCSRF(),
-			},
-			body: JSON.stringify({
-				watchList: list,
-			}),
+		const response = await secureFetch(WATCHLIST_ROUTE, POST, {
+			watchList: list,
 		});
 		const data = await response.json();
 		console.log('SUCCESS::POSTWATCHLIST', data);
@@ -29,7 +41,7 @@ async function postWatchlist(list) {
 async function getQuote(symbol) {
 	let data = null;
 	try {
-		const res = await fetch(`/stocks/quote/${symbol}`);
+		const res = await fetch(`${QUOTE_ROUTE}/${symbol}`);
 		data = await res.json();
 	} catch (err) {
 		console.error('unable to fetch or parse', { err });
@@ -38,29 +50,22 @@ async function getQuote(symbol) {
 }
 
 async function getWatchlist() {
-	const res = await fetch(`/stocks/watchlist`);
+	const res = await fetch(WATCHLIST_ROUTE);
 	return res.json();
 }
 
 async function getAccounts() {
-	const res = await fetch(`/stocks/accounts`);
+	const res = await fetch(ACCOUNT_ROUTE);
 	return await res.json();
 }
 
 async function patchPosition({ account, position }) {
 	try {
-		const response = await fetch(`/stocks/account`, {
-			method: 'PATCH',
-			credentials: 'same-origin',
-			headers: {
-				'Content-Type': 'application/json',
-				'csrf-token': getTokenCSRF(),
-			},
-			body: JSON.stringify({
-				account,
-				position,
-			}),
+		const response = await secureFetch(ACCOUNT_ROUTE, PATCH, {
+			account,
+			position,
 		});
+
 		const data = await response.json();
 		return data;
 	} catch (err) {
@@ -70,18 +75,11 @@ async function patchPosition({ account, position }) {
 
 export async function deletePosition({ holdingId, positionId }) {
 	try {
-		const response = await fetch(`/stocks/account`, {
-			method: 'DELETE',
-			credentials: 'same-origin',
-			headers: {
-				'Content-Type': 'application/json',
-				'csrf-token': getTokenCSRF(),
-			},
-			body: JSON.stringify({
-				holdingId,
-				positionId,
-			}),
+		const response = await secureFetch(ACCOUNT_ROUTE, DELETE, {
+			holdingId,
+			positionId,
 		});
+
 		const data = await response.json();
 		return data;
 	} catch (err) {
