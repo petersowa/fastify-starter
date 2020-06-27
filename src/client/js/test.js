a94ff694f52675384d3c6caf23fda3109179c747
petersowa <petersowa@users.noreply.github.com>
2020-06-26 22:44:32 -0500

    add csrf to form, to check fix on ff

--- src/client/js/test.js
+++ src/client/js/test.js
@@ -50,10 +50,10 @@ const idTable = {
 			submit: (event) => {
 				// event.preventDefault();
 				// const csrf = /_csrf=(\S*);/.exec(document.cookie)[1];
-				const csrf = document
-					.querySelector('meta[name="csrf-token"]')
-					.getAttribute('content');
-				event.target._csrf.value = csrf;
+				// const csrf = document
+				// 	.querySelector('meta[name="csrf-token"]')
+				// 	.getAttribute('content');
+				// event.target._csrf.value = csrf;
 				console.log('submit');
 			},
 		},
