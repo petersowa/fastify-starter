edb523cee9b5115ea37b4d97200c120e3f2f7175
petersowa <petersowa@users.noreply.github.com>
2020-08-06 22:28:22 -0500

    add files for tsconfig
    move styling to separate file for position component

---
+++ src/client/svelte.config.js
@@ -0,0 +1,9 @@
+// svelte.config.js
+import sveltePreprocess from 'svelte-preprocess';
+
+module.exports = {
+	preprocess: sveltePreprocess({
+		// ...svelte-preprocess options
+	}),
+	// ...other svelte options
+};
