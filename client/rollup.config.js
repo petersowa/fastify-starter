6e6517a68da5c4ec0c8f681a02cab5bfbd86668a
petersowa <petersowa@users.noreply.github.com>
2020-08-08 15:17:03 -0500

    code cleanup

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -19,11 +19,8 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
 			dev: !production,
 			extensions: ['.svelte'],
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
 			css: (css) => {
 				css.write('../public/css/main-svelte.css');
 			},
@@ -32,27 +29,12 @@ export default {
 			}),
 		}),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
 			dedupe: ['svelte'],
 		}),
 		commonjs(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		// !production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		// !production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
 		production && terser(),
 		scssPlugin({
 			output: '../public/css/main.css',
@@ -62,24 +44,3 @@ export default {
 		clearScreen: false,
 	},
 };
-
-// function serve() {
-// 	let started = false;
-
-// 	return {
-// 		writeBundle() {
-// 			if (!started) {
-// 				started = true;
-
-// 				require('child_process').spawn(
-// 					'npm',
-// 					['run', 'start', '--', '--dev'],
-// 					{
-// 						stdio: ['ignore', 'inherit', 'inherit'],
-// 						shell: true,
-// 					}
-// 				);
-// 			}
-// 		},
-// 	};
-// }
