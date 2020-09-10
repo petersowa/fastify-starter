7f0c3a2ded9f2f5c484e0370bc1a56d2d6ce8045
petersowa <petersowa@users.noreply.github.com>
2020-09-10 12:43:00 -0500

    add statsModel

---
+++ src/models/statsModel.ts
@@ -0,0 +1,19 @@
+import dbInstance from '../utils/db';
+import { Schema, Document, Types } from 'mongoose';
+import { RapidStatsResult } from '../api/types';
+
+export interface StatsInterface extends Document {
+	symbol: string;
+	date: string;
+	data: RapidStatsResult;
+}
+
+const schema: Schema = new Schema({
+	symbol: { type: String, required: true },
+	date: { type: Date, default: Date.now },
+	data: { type: Object },
+});
+
+const StatsModel = dbInstance.createModel<StatsInterface>('Stats', schema);
+
+export default StatsModel;
