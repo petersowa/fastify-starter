70c659aa75a658680b36c4402736b7f0bb786cb1
petersowa <petersowa@users.noreply.github.com>
2020-09-09 22:48:41 -0500

    refactor hashCache to be generic

--- src/utils/hash-cache.ts
+++ src/utils/hash-cache.ts
@@ -1,19 +1,57 @@
-import { Quote } from '../models/quotesModel';
-
 const MINUTES = 60 * 1000;
 const HOURS = 60 * MINUTES;
 const QUOTE_AGE = 0.5 * HOURS;
 
-export interface HashData {
+export interface HashData<T> {
 	key: string;
-	data?: Quote;
+	data?: T;
 	time: number;
 }
-const hashTable: HashData[] = new Array(541);
 
-const ageStamp = (interval: number = QUOTE_AGE): number => {
-	return Math.floor(Date.now() / interval);
-};
+export class HashCache<T> {
+	private table: HashData<T>[];
+
+	constructor() {
+		this.table = new Array(541);
+	}
+
+	getCache(key: string): HashData<T> | null {
+		const index = this.hashStringToInt(key);
+		const hashTable = this.table;
+
+		if (index in hashTable) {
+			if (isExpired(hashTable[index].time)) return null;
+			const data = hashTable[index];
+			if (data.key === key) return data;
+			console.log('hash collision');
+		}
+		return null;
+	}
+
+	setCache(key: string, data: T): boolean {
+		const index = this.hashStringToInt(key);
+		const hashTable = this.table;
+
+		if (index in hashTable) {
+			if (isExpired(hashTable[index].time)) {
+				hashTable[index] = { key, data, time: Date.now() };
+				return true;
+			}
+			return false;
+		}
+		hashTable[index] = { key, data, time: Date.now() };
+		return true;
+	}
+
+	hashStringToInt: (str: string) => number = (str) => {
+		const hashTable = this.table;
+		let hash = 19;
+		for (const char of str) {
+			hash = (127 * hash + char.charCodeAt(0)) % hashTable.length;
+		}
+		return hash;
+	};
+}
 
 const isExpired = (
 	timeStamp: number,
@@ -22,38 +60,3 @@ const isExpired = (
 	if (Date.now() - timeStamp > interval) return true;
 	return false;
 };
-
-const hashStringToInt: (str: string) => number = (str) => {
-	let hash = 19;
-	for (const char of str) {
-		hash = (127 * hash + char.charCodeAt(0)) % hashTable.length;
-	}
-	return hash;
-};
-
-function getCache(key: string): HashData | null {
-	const index = hashStringToInt(key);
-
-	if (index in hashTable) {
-		if (isExpired(hashTable[index].time)) return null;
-		const data = hashTable[index];
-		if (data.key === key) return data;
-		console.log('hash collision');
-	}
-	return null;
-}
-
-function setCache(key: string, data: Quote): boolean {
-	const index = hashStringToInt(key);
-	if (index in hashTable) {
-		if (isExpired(hashTable[index].time)) {
-			hashTable[index] = { key, data, time: Date.now() };
-			return true;
-		}
-		return false;
-	}
-	hashTable[index] = { key, data, time: Date.now() };
-	return true;
-}
-
-export { getCache, setCache };
