2e5db1d4455dfa9c274a87215fc27656872abc14
petersowa <petersowa@users.noreply.github.com>
2020-09-10 21:22:09 -0500

    code cleanup: remove console.log for symbol

--- src/routes/controllers/quote.ts
+++ src/routes/controllers/quote.ts
@@ -9,7 +9,6 @@ export async function getQuote(
 	request: FastifyRequest,
 	reply: FastifyReply<ServerResponse>
 ): Promise<Quote | null> {
-	console.log(request.params.symbol);
 	const quote = await fetchQuote(request.params.symbol);
 	if (quote === null) reply.status(404).send({ error: 'unable to get data' });
 	return quote;
