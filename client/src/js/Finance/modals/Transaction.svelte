b7b36920afdd63db12f07cd19c87c3dff09dc0de
petersowa <petersowa@users.noreply.github.com>
2020-09-29 22:51:51 -0500

    initial updates to allow typescript svelte

--- client/src/js/Finance/modals/Transaction.svelte
+++ client/src/js/Finance/modals/Transaction.svelte
@@ -1,6 +1,6 @@
-<script>
-	export let toggleModal;
-	export let handleData;
+<script lang="typescript">
+	export let toggleModal: () => void;
+	export let handleData: (e: Event, {}) => void;
 	export let holding;
 	export let position;
 	import Modal from '../../components/modal.svelte';
@@ -22,7 +22,7 @@
 		};
 	});
 
-	function handleSubmit(e) {
+	function handleSubmit(e: Event) {
 		console.log({ formData });
 		// validate
 		// if valid date then send to handle buy form
@@ -37,64 +37,16 @@
 </script>
 
 <style>
-	.buy-title {
-		font-size: 1.2rem;
-	}
-	.buy-form {
-		display: grid;
-		padding: 0.5em;
-		grid-template-columns: 5rem 1fr;
-		grid-template-rows: auto;
-		grid-row-gap: 0.2em;
-	}
-	.form-input {
-		background: #ccc;
-	}
 </style>
 
 <Modal on:close={toggleModal}>
-	<h2 class="buy-title" slot="header">Account Info</h2>
+	<h2 class="buy-title" slot="header">Select Transaction</h2>
 	<form class="buy-form" on:submit|preventDefault={handleSubmit}>
-		<label for="symbol-input">Symbol</label>
-		<input
-			id="symbol-input"
-			tabindex="0"
-			class="form-input"
-			type="text"
-			placeholder="Symbol"
-			bind:value={formData.symbol} />
-		<label for="date-input">Date</label>
-		<input
-			id="date-input"
-			tabindex="0"
-			class="form-input"
-			type="date"
-			placeholder="Date"
-			bind:value={formData.date} />
-		<label for="shares-input">Shares</label>
-		<input
-			id="shares-input"
-			tabindex="0"
-			class="form-input"
-			type="text"
-			placeholder="Shares"
-			bind:value={formData.shares} />
-		<label for="price-input">Price</label>
-		<input
-			id="price-input"
-			tabindex="0"
-			class="form-input"
-			type="text"
-			placeholder="Price"
-			bind:value={formData.price} />
-		<label for="fees-input">Fees</label>
-		<input
-			id="fees-input"
-			tabindex="0"
-			class="form-input"
-			type="text"
-			placeholder="Fees"
-			bind:value={formData.fee} />
-		<button type="submit">Submit</button>
+		<button>Buy</button>
+		<button>Sell</button>
+		<button>Split</button>
+		<button>Dividend</button>
+		<button>Deposit</button>
+		<button>Withdraw</button>
 	</form>
 </Modal>
