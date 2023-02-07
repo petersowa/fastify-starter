876c9e574fc8a2adbef0192e0b53bb8d5efc2d6d
petersowa <petersowa@users.noreply.github.com>
2023-02-07 12:20:48 -0600

    FIX async return

--- src/routes/controllers/quote.js
+++ src/routes/controllers/quote.js
@@ -2,14 +2,16 @@ import { fetchQuote, fetchStats } from '../../api/finance';
 
 export async function getQuote(request, reply) {
 	const quote = await fetchQuote(request.params.symbol);
-	if (quote === null) reply.status(404).send({ error: 'unable to get data' });
+	if (quote === null)
+		return reply.status(404).send({ error: 'unable to get data' });
 	return quote;
 }
 
 export async function getStats(request, reply) {
 	console.log('getting stats for...', request.params.symbol);
 	const stats = await fetchStats(request.params.symbol);
-	if (stats === null) reply.status(404).send({ error: 'unable to get data' });
+	if (stats === null)
+		return reply.status(404).send({ error: 'unable to get data' });
 	return stats;
 }
 
@@ -17,6 +19,7 @@ export async function getHistoricalQuote(request, reply) {
 	console.log(request.params.symbol);
 	const { symbol, date } = request.params;
 	const quote = await fetchQuote(symbol, date);
-	if (quote === null) reply.status(404).send({ error: 'unable to get data' });
+	if (quote === null)
+		return reply.status(404).send({ error: 'unable to get data' });
 	return quote;
 }
