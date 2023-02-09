ebb0c3ac78621fe08176681f7e6d52d2cd6e9b53
petersowa <petersowa@users.noreply.github.com>
2023-02-09 09:41:21 -0600

    REFAC to use Map instead of Objects for data store

--- src/utils/hash-cache.ts
+++ src/utils/hash-cache.ts
@@ -11,23 +11,25 @@ export interface HashData<T> {
 }
 
 export class HashCache<T> {
-	private table: (HashData<T> | HashData<T>[])[];
+	private table: Map<number, HashData<T> | HashData<T>[]>;
 	private maxIntervalAge: number;
+	private maxElements: number;
 
 	constructor(maxIntervalAge = INTERVAL_AGE, hashElements = 541) {
-		this.table = new Array(hashElements);
+		this.table = new Map();
 		this.maxIntervalAge = maxIntervalAge;
+		this.maxElements = hashElements;
 	}
 
 	getCache(key: string): HashData<T> | null {
 		const index = this.hashStringToInt(key);
 		const hashTable = this.table;
 
-		const cached = hashTable[index];
+		const cached = hashTable.get(index);
 
 		if (!cached) return null;
 
-		if (!Array.isArray(cached)) {
+		if (!(cached instanceof Array)) {
 			if (this.isExpired(cached.time)) return null;
 			if (cached.key === key) return cached;
 		} else {
@@ -47,32 +49,33 @@ export class HashCache<T> {
 		const hashTable = this.table;
 		const cacheData = { key, data, time: Date.now() };
 
-		let cached = hashTable[index];
+		let cached = hashTable.get(index);
 
 		if (cached) {
-			if (Array.isArray(cached)) {
+			if (cached instanceof Array) {
 				cached = cached.filter((item) => !this.isExpired(item.time));
 				if (cached.length === 0) {
-					hashTable[index] = cacheData;
+					hashTable.set(index, cacheData);
+				} else {
+					hashTable.set(index, [...cached, cacheData]);
 				}
-				hashTable[index] = [...cached, cacheData];
 			} else {
 				if (this.isExpired(cached.time)) {
-					hashTable[index] = cacheData;
+					hashTable.set(index, cacheData);
 				} else {
-					hashTable[index] = [cached, cacheData];
+					hashTable.set(index, [cached, cacheData]);
 				}
 			}
 		} else {
-			hashTable[index] = { key, data, time: Date.now() };
+			hashTable.set(index, { key, data, time: Date.now() });
 		}
 		return true;
 	}
 
 	list(): void {
-		const { table } = this;
-		table.forEach((cached, i) => {
-			if (Array.isArray(cached)) {
+		// console.log('listing elements', this.table);
+		this.table.forEach((cached, i) => {
+			if (cached instanceof Array) {
 				console.log(
 					i,
 					chalk.blue(cached.map((item) => item.key).join(','))
@@ -84,10 +87,9 @@ export class HashCache<T> {
 	}
 
 	hashStringToInt: (str: string) => number = (str) => {
-		const hashTable = this.table;
 		let hash = 19;
 		for (const char of str) {
-			hash = (127 * hash + char.charCodeAt(0)) % hashTable.length;
+			hash = (127 * (hash + char.charCodeAt(0))) % this.maxElements;
 		}
 		return hash;
 	};
