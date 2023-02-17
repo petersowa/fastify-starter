2641a556bc07e9270fb9aa218dd22c1d829d5b3e
petersowa <petersowa@users.noreply.github.com>
2023-02-17 14:39:09 -0600

    chore: code check and refactor

--- src/server/sessions.ts
+++ src/server/sessions.ts
@@ -1,26 +1,57 @@
 import { FastifyInstance } from 'fastify';
 import { Server, IncomingMessage, ServerResponse } from 'http';
-// import 'fastify-session'; // session types
-
-// import mongoose from 'mongoose';
 
 import fastifySession, { SessionStore } from '@fastify/session';
 import type { Flash } from './flash';
 import { ObjectId } from '@fastify/mongodb';
-// import MongoStore from 'connect-mongo';
+
 import Redis from 'ioredis';
 import connectRedis from 'connect-redis';
 
-const { SESSION_SECRET, NODE_ENV, REDIS_CONNECT } = process.env;
-if (!REDIS_CONNECT) throw new Error('no redis');
+declare module 'fastify' {
+	interface Session {
+		isInitialLoad: boolean;
+		flash: Flash;
+		appState: {
+			modal: string;
+			info: Record<string, unknown>;
+			timeStamp: Date;
+		};
+		isAuth: boolean;
+		username: string;
+		userId: ObjectId;
+	}
+}
 
+const { SESSION_SECRET, NODE_ENV, REDIS_CONNECT } = process.env;
+if (!REDIS_CONNECT) throw new Error('no redis connect string');
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const RedisStore = connectRedis(fastifySession as any);
 console.log({ RedisStore });
 const redisClient = new Redis(REDIS_CONNECT);
-// console.log({ redisClient });
 const redisStore = new RedisStore({
 	client: redisClient,
 }) as unknown as SessionStore;
+const MAX_SESSION_AGE = 2 * 24 * 60 * 60 * 1000;
+
+const register = (
+	app: FastifyInstance<Server, IncomingMessage, ServerResponse>
+): void => {
+	if (!SESSION_SECRET) throw new Error('no session secret found');
+	console.log('REGISTER FASTIFY SESSION');
+	app.register(fastifySession, {
+		cookieName: 'qqSessionId',
+		secret: SESSION_SECRET,
+		saveUninitialized: false,
+		cookie: {
+			path: '/',
+			secure: NODE_ENV === 'production',
+			maxAge: MAX_SESSION_AGE,
+		},
+		store: redisStore,
+	});
+};
 
 async function logRedis() {
 	const stream = redisClient.scanStream({ match: '*', count: 100 });
@@ -48,53 +79,5 @@ async function logRedis() {
 }
 
 logRedis();
-
-declare module 'fastify' {
-	interface Session {
-		isInitialLoad: boolean;
-		flash: Flash;
-		appState: {
-			modal: string;
-			info: Record<string, unknown>;
-			timeStamp: Date;
-		};
-		isAuth: boolean;
-		username: string;
-		userId: ObjectId;
-	}
-}
-
-// import FastifySessionPlugin from '@fastify/session';
-// fastifySession.MemoryStore = {}; // MongoStore fails if not added
-
-const MAX_SESSION_AGE = 2 * 24 * 60 * 60 * 1000;
-
-// interface Session {
-// 	user_id: string;
-// 	other_key: your_prefer_type;
-// 	id?: number;
-// }
-
-const register = (
-	app: FastifyInstance<Server, IncomingMessage, ServerResponse>
-): void => {
-	if (!SESSION_SECRET) throw new Error('no session secret found');
-	console.log('REGISTER FASTIFY SESSION');
-	// console.log({ app, redisStore: redisStore });
-	app.register(fastifySession, {
-		cookieName: 'qqSessionId',
-		secret: SESSION_SECRET,
-		saveUninitialized: false,
-		cookie: {
-			path: '/',
-			secure: NODE_ENV === 'production',
-			maxAge: MAX_SESSION_AGE,
-		},
-		store: redisStore,
-		// store: MongoStore.create({
-		// 	mongoUrl: process.env.MONGODB_NAME,
-		// }) as unknown as SessionStore,
-	});
-};
 
 export default register;
